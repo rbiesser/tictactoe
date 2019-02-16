e371bb00e641d974c197781a96ed9bbd277e650b
Robbie Biesser <rbiesser@gmail.com>
2019-02-15 21:41:45 -0500

    you can now play a full game and determine who wins

--- tictactoe.c
+++ tictactoe.c
@@ -24,6 +24,9 @@ void printBoard(char * board);
 /* We need a method for the player to select a square */
 int chooseSquare(char * board, char player, int choice);
 
+/* We need a method to determine when a player has won */
+int playerWins(char * board, char player);
+
 /* the players will either be an X or an O and represented as a macro */
 #define player1 'X'
 #define player2 'O'
@@ -42,22 +45,96 @@ int main()
 	// variable to hold the player's selection from the board
 	int choice;
 
-	// displaying the board is as easy as calling the printBoard function
+	// variable to track how may turns are left in this game
+	int openSquares = 9;
+
+	// keep alternating between player turns until one wins or there are no more squares left
+	while (openSquares > 0 && !playerWins(board, currentPlayer))
+	{
+
+		// next player
+		currentPlayer = (currentPlayer == player1) ? player2 : player1;
+
+		do // chooseSquare will make sure the player makes a valid move or gives them another chance.
+		{
+			// give notice of the current player
+			printf("\n%c to play...\n", currentPlayer);
+
+			// displaying the board is as easy as calling the printBoard function
+			// the board prints with the current state
+			printBoard(board);
+
+			// prompt the user for input
+			printf("%s", "Choose a square on the board (1-9) ");
+			scanf("%d", &choice);
+
+				// the array is zero based but the player selects 1-9
+		} while (!chooseSquare(board, currentPlayer, choice-1));
+
+		// decreasing the number of squares available after each turn
+		openSquares--;
+	}
+
+	// print the final game board
 	printBoard(board);
 
-	// prompt the user for input
-	printf("%s", "Choose a square on the board (1-9) ");
-	scanf("%d", &choice);
-
-	// the array is zero based but the player selects 1-9
-	chooseSquare(board, currentPlayer, choice-1);
-
-	// display the changed board
-	printBoard(board);
+	// if a player didn't win last turn, we know the board ran out of squares
+	if (!playerWins(board, currentPlayer))
+	{
+		printf("\n%s\n", "This game is cat. =^_^= Play again.");
+	}
 
 	return 1;
 
 	// end main
+}
+
+/**
+	We need a method to determine when a player has won 
+
+	There are 8 ways to win
+
+	1 2 3 = 6
+	4 5 6 = 15
+	7 8 9 = 24
+	1 4 7 = 12
+	2 5 8 = 15
+	3 6 9 = 18
+	1 5 9 = 15
+	3 5 7 = 15
+
+	@param the current player
+	@return true when a player wins
+*/
+int playerWins(char * board, char player)
+{
+	// brute force would have you check every square in order
+	// but we can use short circuit logic to find a winner.
+
+	if (
+		// zero based
+		// horizontal
+		(board[0] == player && board[1] == player && board[2] == player) ||
+		(board[3] == player && board[4] == player && board[5] == player) ||
+		(board[6] == player && board[7] == player && board[8] == player) ||
+
+		// vertical
+		(board[0] == player && board[3] == player && board[6] == player) ||
+		(board[1] == player && board[4] == player && board[7] == player) ||
+		(board[2] == player && board[5] == player && board[8] == player) ||
+
+		// diagonal
+		(board[0] == player && board[4] == player && board[8] == player) ||
+		(board[2] == player && board[4] == player && board[6] == player)
+		)
+	{
+		printf("\n%c WINS!\n", player);
+
+		return 1; // exit loop and end game
+	}
+
+	return 0; // default to false
+
 }
 
 /**
@@ -91,11 +168,11 @@ int chooseSquare(char * board, char player, int choice)
 			|		|
 		1	|	2	|	3
 			|		|
-	--------------------------
+	-------------------------
 			|		|
 		4	|	5	|	6
 			|		|
-	--------------------------
+	-------------------------
 			|		|
 		7	|	8	|	9
 			|		|
